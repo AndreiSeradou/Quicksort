43e08662d9352bb9a66e5ab2bdd992430651fb7b
AndreiSeradou <andreiseradou@gmail.com>
2021-07-07 09:08:35 +0300

    Create project

---
+++ Quicksort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace Quicksort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Quicksort<T>
+    {
+        public void doQuicksort(ref T[] array)
+        {
+
+        }
+
+        public void doQuicksort(ref List<T> list)
+        {
+            
+        }
+
+        private void doQuicksort(ref T begin,ref T end)
+        {
+
+        }
+
+
+}
