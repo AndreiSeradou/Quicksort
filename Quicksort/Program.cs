ce25614cac39703ef0a1012b383f298f5a6d1214
AndreiSeradou <andreiseradou@gmail.com>
2021-07-07 23:41:56 +0300

    ReadyTask

--- Quicksort/Program.cs
+++ Quicksort/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Numerics;
-using System.Collections.Generic;
 
 namespace Quicksort
 {
@@ -11,23 +9,5 @@ namespace Quicksort
             Console.WriteLine("Hello World!");
         }
     }
-
-    class Quicksort<T>
-    {
-        public void doQuicksort(ref T[] array)
-        {
-
-        }
-
-        public void doQuicksort(ref List<T> list)
-        {
-            
-        }
-
-        private void doQuicksort(ref T begin,ref T end)
-        {
-
-        }
-
 
 }
---
+++ Quicksort/Sort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quicksort
+{
+    internal class Sort
+    {
+        public static void ShakerSort<T>(ref List<T> list)
+            where T : IComparable<T>
+        {
+            if (list.Count == 0)
+            {
+                return;
+            }
+            int left = 0;
+            int right = list.Count - 1;
+            while (left <= right)
+            {
+                for (int i = right; i > left; --i)
+                {
+                    if (list[i - 1].CompareTo(list[i]) > 0)
+                    {
+                        T temp;
+                        temp = list[i - 1];
+                        list[i - 1] = list[i];
+                        list[i] = temp;
+                    }
+                }
+                ++left;
+                for (int i = left; i < right; ++i)
+                {
+                    if (list[i].CompareTo(list[i + 1]) > 0)
+                    {
+                        T temp;
+                        temp = list[i - 1];
+                        list[i - 1] = list[i];
+                        list[i] = temp;
+                    }
+                }
+                --right;
+            }
+        }
+    }
+}
